cc5978e893bc41abcfe60773fa5b69c804357d71
İBRAHİM HALİL GUNDUZ <ibrahimhalilgunduzz@gmail.com>
2023-01-28 01:17:24 +0300

    react projesi oluşturuldu.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "Ali Veli";
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ali Veli <code>src/App.js</code> and save to reload.
         </p>
+        <h1>{name}</h1>  
         <a
           className="App-link"
           href="https://reactjs.org"
